a3d3b83c76d2ad8289a598bc6cad9193fb6a1d3f
GG <46676047+EstamelGG@users.noreply.github.com>
2024-12-01 00:45:58 +0800

    update

--- Neocom III/Setting/SelectLanguage.swift
+++ Neocom III/Setting/SelectLanguage.swift
@@ -6,8 +6,12 @@ struct SelectLanguageView: View {
         "English",
         "中文"
     ]
+    
     // 跟踪用户选择的语言
-    @State private var selectedLanguageIndex: Int? = nil
+    @State private var selectedLanguageIndex: Int?
+    
+    // 使用 @AppStorage 来持久化存储用户选择的语言
+    @AppStorage("selectedLanguage") var selectedLanguage: String?
     
     var body: some View {
         List(languages.indices, id: \.self) { index in
@@ -24,14 +28,24 @@ struct SelectLanguageView: View {
             .contentShape(Rectangle())  // 确保点击区域完整
             .onTapGesture {
                 // 用户点击时更新选择的语言
-                if selectedLanguageIndex == index {
-                    selectedLanguageIndex = nil  // 如果点击已选中项，取消选择
-                } else {
-                    selectedLanguageIndex = index  // 否则设置为新的选择
-                }
+                selectedLanguage = languages[index]
+                selectedLanguageIndex = index
             }
         }
-        .navigationTitle(NSLocalizedString("Main_Setting_Select Language", comment: ""))  // 设置标题
+        .navigationTitle(NSLocalizedString("Main_Setting_Select Language", comment: ""))
+        .onAppear {
+            // 默认选择当前存储的语言
+            if let language = selectedLanguage {
+                if let index = languages.firstIndex(of: language) {
+                    selectedLanguageIndex = index
+                }
+            } else {
+                // 如果没有选择语言，则根据系统语言设置默认值
+                let systemLanguage = Locale.preferredLanguages.first ?? "en"
+                selectedLanguage = systemLanguage.starts(with: "zh") ? "中文" : "English"
+                selectedLanguageIndex = systemLanguage.starts(with: "zh") ? 1 : 0
+            }
+        }
     }
 }
 
