2f389ee269ba914d32232cfc38e74202ab54e568
GG <46676047+EstamelGG@users.noreply.github.com>
2024-12-03 14:16:05 +0800

    Update Neocom_IIIApp.swift

--- Neocom III/Neocom_IIIApp.swift
+++ Neocom III/Neocom_IIIApp.swift
@@ -7,45 +7,37 @@ struct Neocom_IIIApp: App {
     @AppStorage("selectedLanguage") private var selectedLanguage: String?
     @StateObject private var databaseManager = DatabaseManager() // 共享的数据库管理器
 
-    // Initialize language settings
     init() {
+        // 设置语言
+        configureLanguage()
+        // 解压图标文件
+        decompressIconsZip()
+    }
+
+    private func configureLanguage() {
         if let language = selectedLanguage {
-            // Set the app language
             UserDefaults.standard.set([language], forKey: "AppleLanguages")
             UserDefaults.standard.synchronize()
         } else {
-            // Use the system default language
             let systemLanguage = Locale.preferredLanguages.first ?? "en"
             UserDefaults.standard.set([systemLanguage], forKey: "AppleLanguages")
             UserDefaults.standard.synchronize()
         }
-
-        // Decompress the icons.zip file only if the Icons folder doesn't exist
-        decompressIconsZip()
-
-        // Load the database when the app starts
-        databaseManager.loadDatabase()
     }
 
-    // Decompress icons.zip file (only if the folder doesn't exist)
     private func decompressIconsZip() {
-        // Get the path to the app's resources
         guard let zipFilePath = Bundle.main.url(forResource: "icons", withExtension: "zip") else {
             print("icons.zip file not found")
             return
         }
 
-        // Get the destination folder for the unzipped files
         let destinationPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("Icons")
 
-        // Check if the destination folder exists
         if FileManager.default.fileExists(atPath: destinationPath.path) {
-            // If the folder exists, don't unzip again
             print("Icons folder already exists, skipping extraction.")
             return
         }
 
-        // If the folder doesn't exist, unzip the file
         do {
             try Zip.unzipFile(zipFilePath, destination: destinationPath, overwrite: true, password: nil)
             print("Successfully unzipped icons.zip to \(destinationPath.path)")
@@ -56,10 +48,9 @@ struct Neocom_IIIApp: App {
 
     var body: some Scene {
         WindowGroup {
-            // 将 databaseManager 作为环境对象传递给 ContentView
             ContentView(databaseManager: databaseManager)
                 .onAppear {
-                    // 确保数据库在应用启动时加载
+                    // 在应用启动时加载数据库
                     databaseManager.loadDatabase()
                 }
         }
