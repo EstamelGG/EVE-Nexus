e376487194dd7aa61b8801542679d99bad0fcac7
GG <46676047+EstamelGG@users.noreply.github.com>
2024-12-04 15:06:29 +0800

    Update DBQuery.swift

--- Neocom III/utils/DBQuery.swift
+++ Neocom III/utils/DBQuery.swift
@@ -1,7 +1,18 @@
 import SQLite3
 
-// 通用的 SQL 查询函数
-public func executeQuery<T>(db: OpaquePointer, query: String, bind: ((OpaquePointer) -> Void)? = nil, resultProcessor: (OpaquePointer) -> T?) -> [T] {
+/// 通用的 SQL 查询函数
+/// - Parameters:
+///   - db: 数据库指针
+///   - query: 要执行的 SQL 查询语句
+///   - bind: 可选的绑定参数闭包（默认为 `nil`），用于在查询中绑定变量
+///   - resultProcessor: 结果处理闭包，用于将每一行的结果转换为指定类型
+/// - Returns: 返回一个包含查询结果的数组
+public func executeQuery<T>(
+    db: OpaquePointer,
+    query: String,
+    bind: ((OpaquePointer) -> Void)? = nil,
+    resultProcessor: @escaping (OpaquePointer) -> T?
+) -> [T] {
     var results: [T] = []
     var statement: OpaquePointer?
 
@@ -18,9 +29,13 @@ public func executeQuery<T>(db: OpaquePointer, query: String, bind: ((OpaquePoin
                 results.append(result)
             }
         }
+
+        // 释放 statement 资源
         sqlite3_finalize(statement)
     } else {
-        print("Failed to prepare statement")
+        // 查询准备失败时打印错误信息
+        let errorMessage = String(cString: sqlite3_errmsg(db))
+        print("Failed to prepare statement: \(errorMessage)")
     }
 
     return results
