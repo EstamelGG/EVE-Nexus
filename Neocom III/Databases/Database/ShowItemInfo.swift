d2fd1d0638c9ece47348d5cfad9c9614578ab7a0
GG <46676047+EstamelGG@users.noreply.github.com>
2024-12-04 11:22:28 +0800

    Update ShowItemInfo.swift

--- Neocom III/Databases/Database/ShowItemInfo.swift
+++ Neocom III/Databases/Database/ShowItemInfo.swift
@@ -10,9 +10,24 @@ struct ItemDetails {
     let categoryName: String
 }
 
-// 处理换行的函数
-func fix_newline(_ text: String) -> String {
-    return text.replacingOccurrences(of: "\n\n\n+", with: "\n\n", options: .regularExpression)
+// 用于过滤 HTML 标签并处理换行的函数
+func filterText(_ text: String) -> String {
+    // 1. 替换 <b> 和 </b> 标签为一个空格
+    var filteredText = text.replacingOccurrences(of: "<b>", with: " ")
+    filteredText = filteredText.replacingOccurrences(of: "</b>", with: " ")
+    filteredText = filteredText.replacingOccurrences(of: "<br>", with: "\n")
+    // 2. 替换 <link> 和 </link> 标签为一个空格
+    filteredText = filteredText.replacingOccurrences(of: "<link.*?>", with: " ", options: .regularExpression)
+    filteredText = filteredText.replacingOccurrences(of: "</link>", with: " ", options: .regularExpression)
+
+    // 3. 删除其他 HTML 标签
+    let regex = try! NSRegularExpression(pattern: "<(?!b|link)(.*?)>", options: .caseInsensitive)
+    filteredText = regex.stringByReplacingMatches(in: filteredText, options: [], range: NSRange(location: 0, length: filteredText.utf16.count), withTemplate: "")
+
+    // 4. 替换多个连续的换行符为一个换行符
+    filteredText = filteredText.replacingOccurrences(of: "\n\n+", with: "\n\n", options: .regularExpression)
+
+    return filteredText
 }
 
 // ShowItemInfo view
@@ -43,7 +58,7 @@ struct ShowItemInfo: View {
                     }
                     .padding(.vertical, 8)
                     
-                    Text(fix_newline(itemDetails.description))
+                    Text(filterText(itemDetails.description))
                         .font(.body)
                         .foregroundColor(.primary)
                 }
