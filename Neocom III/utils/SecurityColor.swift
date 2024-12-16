b14df0ee4f554e3931d9fef2c3f8e16b6c56069c
GG <46676047+EstamelGG@users.noreply.github.com>
2024-12-16 13:23:28 +0800

    Update SecurityColor.swift

--- Neocom III/utils/SecurityColor.swift
+++ Neocom III/utils/SecurityColor.swift
@@ -29,10 +29,8 @@ func getSecurityColor(_ security: Double) -> Color {
         return Color(red: 114/255, green: 204/255, blue: 237/255)
     case ...0.7 where truncatedSecurity > 0.6:
         return Color(red: 129/255, green: 216/255, blue: 169/255)
-    case ...0.6 where truncatedSecurity > 0.5:
+    case ...0.6 where truncatedSecurity > 0.4:
         return Color(red: 143/255, green: 225/255, blue: 103/255)
-    case ...0.5 where truncatedSecurity > 0.4:
-        return Color(red: 242/255, green: 254/255, blue: 149/255)
     case ...0.4 where truncatedSecurity > 0.0:
         return Color(red: 208/255, green: 113/255, blue: 45/255)
     default:
