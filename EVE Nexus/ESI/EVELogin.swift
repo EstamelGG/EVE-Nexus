e2cc8fe74430a3831611939362bd9d27900d1eab
GG <46676047+EstamelGG@users.noreply.github.com>
2024-12-21 09:37:39 +0800

    Update EVELogin.swift

--- EVE Nexus/ESI/EVELogin.swift
+++ EVE Nexus/ESI/EVELogin.swift
@@ -312,7 +312,7 @@ class EVELogin {
         
         // 更新当前技能信息
         if let currentSkill = skillQueue.first(where: { $0.queue_position == 0 }) {
-            if let skillName = await NetworkManager.getSkillName(
+            if let skillName = NetworkManager.getSkillName(
                 skillId: currentSkill.skill_id,
                 databaseManager: databaseManager
             ) {
@@ -713,7 +713,7 @@ class EVELogin {
     
     // 刷新令牌
     func refreshToken(refreshToken: String, force: Bool = false) async throws -> EVEAuthToken {
-        // 如果不是强制刷新，检查上次更新时间
+        // 如果不是强制刷新，检查上次更��时间
         if !force {
             if let characters = try? JSONDecoder().decode([CharacterAuth].self, from: UserDefaults.standard.data(forKey: charactersKey) ?? Data()),
                let character = characters.first(where: { $0.token.refresh_token == refreshToken }),
