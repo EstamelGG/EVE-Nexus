d1ad1a9ea624571808f1c6b61947520c92a7f7c8
GG <46676047+EstamelGG@users.noreply.github.com>
2024-12-23 17:43:42 +0800

    Update CharacterWalletAPI.swift

--- EVE Nexus/API/Private_ESI/CharacterWalletAPI.swift
+++ EVE Nexus/API/Private_ESI/CharacterWalletAPI.swift
@@ -11,7 +11,7 @@ class CharacterWalletAPI {
     
     // 内存缓存
     private var memoryCache: [Int: CacheEntry] = [:]
-    private let cacheTimeout: TimeInterval = 300 // 5分钟缓存
+    private let cacheTimeout: TimeInterval = 30 * 60 // 30分钟缓存
     
     // UserDefaults键前缀
     private let walletCachePrefix = "wallet_cache_"
