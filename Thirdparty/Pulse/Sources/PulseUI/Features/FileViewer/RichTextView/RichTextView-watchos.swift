ac081a05bbb45a00de56c98497569b3e36c93f1d
GG <46676047+EstamelGG@users.noreply.github.com>
2024-12-24 15:12:57 +0800

    Revert "Revert "Load depo""
    This reverts commit 2045780ca4926a8b0b06d0e0c7f969f809cd5b25.

---
+++ Thirdparty/Pulse/Sources/PulseUI/Features/FileViewer/RichTextView/RichTextView-watchos.swift
@@ -0,0 +1,53 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2020-2024 Alexander Grebenyuk (github.com/kean).
+
+import SwiftUI
+import Pulse
+
+#if os(watchOS) || os(tvOS) || os(macOS)
+
+struct RichTextView: View {
+    let viewModel: RichTextViewModel
+
+    var body: some View {
+        ScrollView {
+            if let string = viewModel.attributedString {
+                Text(string)
+            } else {
+                Text(viewModel.text)
+            }
+        }
+#if os(watchOS)
+        .toolbar {
+            if #available(watchOS 9, *) {
+                ShareLink(item: viewModel.text)
+            }
+        }
+#endif
+    }
+}
+
+final class RichTextViewModel: ObservableObject {
+    let text: String
+    let attributedString: AttributedString?
+
+    var isLinkDetectionEnabled = true
+    var isEmpty: Bool { text.isEmpty }
+
+    init(string: String) {
+        self.text = string
+        self.attributedString = nil
+    }
+
+    init(string: NSAttributedString, contentType: NetworkLogger.ContentType? = nil) {
+#if os(macOS)
+        self.attributedString = try? AttributedString(string, including: \.appKit)
+#else
+        self.attributedString = try? AttributedString(string, including: \.uiKit)
+#endif
+        self.text = string.string
+    }
+}
+
+#endif
