fd01417b422bbb61ee80561027634a401ae6b61f
GG <46676047+EstamelGG@users.noreply.github.com>
2025-01-05 17:37:52 +0800

    Update CharacterMailListView.swift

--- EVE Nexus/Character/Mail/CharacterMailListView.swift
+++ EVE Nexus/Character/Mail/CharacterMailListView.swift
@@ -279,7 +279,7 @@ private struct MailListItemView: View {
     var body: some View {
         HStack(spacing: 12) {
             // 发件人头像
-            CharacterPortrait(characterId: mail.from, size: 64)
+            CharacterPortrait(characterId: mail.from, size: 48)
             
             VStack(alignment: .leading, spacing: 2) {
                 // 邮件主题
