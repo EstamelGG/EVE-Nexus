489e9b703b0d16106b153d1a98afd1d7ebc5b097
GG <46676047+EstamelGG@users.noreply.github.com>
2025-01-06 10:57:18 +0800

    Update MiningLedgerView.swift

--- EVE Nexus/Business/Mining/MiningLedgerView.swift
+++ EVE Nexus/Business/Mining/MiningLedgerView.swift
@@ -175,9 +175,15 @@ struct MiningLedgerView: View {
     var body: some View {
         List {
             if viewModel.isLoading {
-                ProgressView(NSLocalizedString("Mining_Loading", comment: ""))
-                    .frame(maxWidth: .infinity)
-            } else if viewModel.monthGroups.isEmpty {
+                Section {
+                    HStack {
+                        Spacer()
+                        ProgressView(NSLocalizedString("Mining_Loading", comment: ""))
+                        Spacer()
+                    }
+                    .listRowBackground(Color.clear)
+                }
+            } else if !viewModel.isLoading && viewModel.monthGroups.isEmpty {
                 Section {
                     HStack {
                         Spacer()
@@ -186,7 +192,7 @@ struct MiningLedgerView: View {
                                 .font(.system(size: 30))
                                 .foregroundColor(.gray)
                             Text(NSLocalizedString("Orders_No_Data", comment: ""))
-                            .foregroundColor(.gray)
+                                .foregroundColor(.gray)
                         }
                         .padding()
                         Spacer()
