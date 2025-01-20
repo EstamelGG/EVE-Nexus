094e18672bf1c60c0ea73ac4f5978fd420e70236
GG <46676047+EstamelGG@users.noreply.github.com>
2025-01-20 15:11:30 +0800

    Update ExtractorYieldCalculator.swift

--- EVE Nexus/Business/Planetary/ExtractorYieldCalculator.swift
+++ EVE Nexus/Business/Planetary/ExtractorYieldCalculator.swift
@@ -203,7 +203,7 @@ struct ExtractorYieldChartView: View {
                             ForEach(yields, id: \.cycle) { yield in
                                 Rectangle()
                                     .fill(yield.cycle == currentCycle + 1 ? Color.blue : Color.gray.opacity(0.6))
-                                    .frame(width: max(1, (geometry.size.width - CGFloat(yields.count)) / CGFloat(yields.count)),
+                                    .frame(width: geometry.size.width / CGFloat(yields.count),
                                            height: CGFloat(yield.yield) / CGFloat(maxYield) * chartHeight)
                             }
                         }
